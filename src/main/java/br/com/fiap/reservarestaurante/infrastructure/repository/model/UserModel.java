3268b299d3d5a3f660212464af6a0fc6421c4ec4
Ariel Neumeyer <arielneu@outlook.com>
2025-03-11 09:20:12 -0300

    User GatewayImpl

---
+++ src/main/java/br/com/fiap/reservarestaurante/infrastructure/repository/model/UserModel.java
@@ -0,0 +1,25 @@
+package br.com.fiap.reservarestaurante.infrastructure.repository.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class UserModel extends Model<String> {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String surname;
+    @NotNull
+    private int age;
+    @Email
+    @NotBlank
+    private String email;
+}
