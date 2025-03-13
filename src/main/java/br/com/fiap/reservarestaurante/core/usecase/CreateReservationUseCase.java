ed7c0a6c5f927b44eaa2a4c5a50b47969c276035
evignacio <reignomo27@hotmail.com>
2025-03-13 18:58:53 -0300

    Ajustando fluxo de reserva

--- src/main/java/br/com/fiap/reservarestaurante/core/usecase/CreateReservationUseCase.java
+++ src/main/java/br/com/fiap/reservarestaurante/core/usecase/CreateReservationUseCase.java
@@ -2,19 +2,28 @@ package br.com.fiap.reservarestaurante.core.usecase;
 
 import br.com.fiap.reservarestaurante.core.domain.Reservation;
 import br.com.fiap.reservarestaurante.core.dto.ReservationDTO;
-import br.com.fiap.reservarestaurante.core.gateway.ReservationGateway;
+import br.com.fiap.reservarestaurante.core.gateway.RestaurantGateway;
 
 public class CreateReservationUseCase {
 
-    private final ReservationGateway reservationGateway;
+    private final RestaurantGateway restaurantGateway;
 
-    public CreateReservationUseCase(ReservationGateway reservationGateway) {
-        this.reservationGateway = reservationGateway;
+    public CreateReservationUseCase(RestaurantGateway restaurantGateway) {
+        this.restaurantGateway = restaurantGateway;
     }
 
     public ReservationDTO execute(ReservationDTO reservationDTO) {
-        var reservation = new Reservation(reservationDTO.idRestaurant(), reservationDTO.idUser(), reservationDTO.numberOfClients(), reservationDTO.date());
-        reservationGateway.save(reservation);
+        var reservation = new Reservation(
+                reservationDTO.idRestaurant(),
+                reservationDTO.idUser(),
+                reservationDTO.amountOfTables(),
+                reservationDTO.date()
+        );
+        var restaurant = restaurantGateway.findById(reservationDTO.idRestaurant())
+                .orElseThrow(() -> new IllegalStateException("Restaurant not found"));
+
+        restaurant.addReservation(reservation);
+        restaurantGateway.save(restaurant);
         return reservationDTO;
     }
 }
--- src/test/java/br/com/fiap/reservarestaurante/core/usecase/CreateReservationUseCaseTest.java
+++ src/test/java/br/com/fiap/reservarestaurante/core/usecase/CreateReservationUseCaseTest.java
@@ -1,46 +1,77 @@
 package br.com.fiap.reservarestaurante.core.usecase;
 
-import br.com.fiap.reservarestaurante.core.domain.Reservation;
+import br.com.fiap.reservarestaurante.core.domain.*;
 import br.com.fiap.reservarestaurante.core.dto.ReservationDTO;
-import br.com.fiap.reservarestaurante.core.gateway.ReservationGateway;
+import br.com.fiap.reservarestaurante.core.gateway.RestaurantGateway;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CreateReservationUseCaseTest {
 
     @Mock
-    private ReservationGateway reservationGateway;
+    private RestaurantGateway restaurantGateway;
 
     @InjectMocks
     private CreateReservationUseCase createReservationUseCase;
 
     @Test
     void shouldCreateReservation() {
-        var idRestaurant = "1";
-        var idUser = "2";
-        var numberOfClients = 4;
-        var date = LocalDateTime.now();
-        var reservationReq = new ReservationDTO(idRestaurant, idUser, numberOfClients, date);
+        Address address = new Address("São Paulo", "SP", "Brazil", "Rua A", 123, "12345678");
+        Category category = new Category("Italian");
+        Set<WorkPeriod> workPeriods = new HashSet<>();
+        var startHour = 10;
+        var endHour = 22;
+        workPeriods.add(new WorkPeriod(DayOfWeek.MONDAY, startHour, endHour));
+        workPeriods.add(new WorkPeriod(DayOfWeek.TUESDAY, startHour, endHour));
+        workPeriods.add(new WorkPeriod(DayOfWeek.WEDNESDAY, startHour, endHour));
+        workPeriods.add(new WorkPeriod(DayOfWeek.THURSDAY, startHour, endHour));
+        workPeriods.add(new WorkPeriod(DayOfWeek.FRIDAY, startHour, endHour));
+        workPeriods.add(new WorkPeriod(DayOfWeek.SATURDAY, startHour, endHour));
+        workPeriods.add(new WorkPeriod(DayOfWeek.SUNDAY, startHour, endHour));
+        var resturant = new Restaurant("Restaurant Name", address, 50, category, workPeriods);
+
+        var dataReserva = LocalDateTime.now().withHour(12);
+        var amountOfTables = 2;
+        var reservationReq = new ReservationDTO(resturant.getId(), "idUser", amountOfTables, dataReserva);
+
+        when(restaurantGateway.findById(resturant.getId())).thenReturn(Optional.of(resturant));
 
         var result = assertDoesNotThrow(() -> createReservationUseCase.execute(reservationReq));
-        verify(reservationGateway, times(1)).save(any(Reservation.class));
+        verify(restaurantGateway, times(1)).save(any(Restaurant.class));
         assertThat(result).isNotNull();
-        assertThat(result.date()).isEqualTo(date);
-        assertThat(result.idRestaurant()).isEqualTo(idRestaurant);
-        assertThat(result.idUser()).isEqualTo(idUser);
-        assertThat(result.numberOfClients()).isEqualTo(numberOfClients);
+        assertThat(result.date()).isEqualTo(dataReserva);
+        assertThat(result.idRestaurant()).isEqualTo(reservationReq.idRestaurant());
+        assertThat(result.idUser()).isEqualTo(reservationReq.idUser());
+        assertThat(result.amountOfTables()).isEqualTo(amountOfTables);
 
+    }
+
+    @Test
+    void shouldReturnExceptionRestaurantNotFound() {
+        var dataReserva = LocalDateTime.now().withHour(12);
+        var amountOfTables = 2;
+        var reservationReq = new ReservationDTO("restaurantId", "idUser", amountOfTables, dataReserva);
+
+        when(restaurantGateway.findById(reservationReq.idRestaurant())).thenReturn(Optional.empty());
+        var exception = assertThrows(IllegalStateException.class, () -> createReservationUseCase.execute(reservationReq));
+
+        assertThat(exception).isInstanceOf(IllegalStateException.class)
+                .hasMessage("Restaurant not found");
     }
 }
