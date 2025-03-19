f137ad39e208a1d6eb0ee31fac18d5eea364cf02
Ariel Neumeyer <arielneu@outlook.com>
2025-03-19 09:14:05 -0300

    Construtores para criacao de restaurante startup

--- src/main/java/br/com/fiap/reservarestaurante/infrastructure/repository/model/WorkPeriodModel.java
+++ src/main/java/br/com/fiap/reservarestaurante/infrastructure/repository/model/WorkPeriodModel.java
@@ -1,6 +1,7 @@
 package br.com.fiap.reservarestaurante.infrastructure.repository.model;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,9 +9,14 @@ import java.time.DayOfWeek;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class WorkPeriodModel {
     @NotNull
     private DayOfWeek dayOfWeek;
     private int startHour;
     private int endHour;
+
+    public WorkPeriodModel() {
+        //Default
+    }
 }
