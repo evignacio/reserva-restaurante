f2a8133a459bfb3f9ae89125d89b8c677bb94d00
Ariel Neumeyer <arielneu@outlook.com>
2025-03-25 09:01:24 -0300

    doc sagger

--- src/main/java/br/com/fiap/reservarestaurante/infrastructure/controller/ReservationController.java
+++ src/main/java/br/com/fiap/reservarestaurante/infrastructure/controller/ReservationController.java
@@ -6,6 +6,11 @@ import br.com.fiap.reservarestaurante.core.usecase.CreateReservationUseCase;
 import br.com.fiap.reservarestaurante.core.usecase.FinalizeReservationUseCase;
 import br.com.fiap.reservarestaurante.core.usecase.FindUserReservationsUseCase;
 import br.com.fiap.reservarestaurante.infrastructure.controller.response.ServiceResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,18 +30,39 @@ public class ReservationController {
         this.findUserReservationsUseCase = findUserReservationsUseCase;
     }
 
+    @Operation(summary = "Reservar mesa em restaurante")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reserva criada",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ServiceResponse.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal Error",
+                    content = @Content) })
     @PostMapping("reservations")
     public ResponseEntity<ServiceResponse<ReservationDTO>> createReservation(@RequestBody CreateReservationDTO reservationDTO) {
         var reservation = createReservationUseCase.execute(reservationDTO);
         return ResponseEntity.ok(ServiceResponse.build(reservation));
     }
 
+    @Operation(summary = "Finalizar uma reserva existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reserva finalizada",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ServiceResponse.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal Error",
+                    content = @Content) })
     @PatchMapping("reservations/{idReservation}/restaurants/{idRestaurant}")
     public ResponseEntity<ServiceResponse<Void>> finalizeReservation(@PathVariable String idRestaurant, @PathVariable String idReservation) {
         finalizeReservationUseCase.execute(idReservation, idRestaurant);
         return ResponseEntity.ok(ServiceResponse.build());
     }
 
+    @Operation(summary = "Buscar reserva por id de usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reserva encontrada",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ServiceResponse.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal Error",
+                    content = @Content) })
     @GetMapping("reservations/users/{idUser}")
     public ResponseEntity<ServiceResponse<Set<ReservationDTO>>> findUserReservation(@PathVariable String idUser) {
         return ResponseEntity.ok(ServiceResponse.build(findUserReservationsUseCase.execute(idUser)));
--- src/main/java/br/com/fiap/reservarestaurante/infrastructure/controller/ReviewController.java
+++ src/main/java/br/com/fiap/reservarestaurante/infrastructure/controller/ReviewController.java
@@ -3,6 +3,11 @@ package br.com.fiap.reservarestaurante.infrastructure.controller;
 import br.com.fiap.reservarestaurante.core.dto.ReviewDTO;
 import br.com.fiap.reservarestaurante.core.usecase.CreateReviewUseCase;
 import br.com.fiap.reservarestaurante.infrastructure.controller.response.ServiceResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +25,13 @@ public class ReviewController {
         this.createReviewUseCase = createReviewUseCase;
     }
 
+    @Operation(summary = "Avaliar restaurante")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "ok",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ServiceResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal Error",
+                    content = @Content)})
     @PostMapping()
     public ResponseEntity<ServiceResponse<Void>> createReview(@RequestBody ReviewDTO reviewDTO) {
         createReviewUseCase.execute(reviewDTO);
